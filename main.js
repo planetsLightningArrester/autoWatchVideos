f2e1b115430af494b210231e4f34cf4d313c5f16
Planets Lightning Arrester <franciscogssm94@gmail.com>
2021-07-17 14:34:15 -0300

    fc

---
+++ main.js
@@ -0,0 +1,224 @@
+// let urls = [
+  // "https://watch.lolesports.com/vod/104202471517814535/1",
+  // "https://watch.lolesports.com/vod/104202471517814525/1",
+  // "https://watch.lolesports.com/vod/104202471517814549/1",
+  // "https://watch.lolesports.com/vod/104202471517814543/1",
+  // "https://watch.lolesports.com/vod/104202471517814545/1",
+  // "https://watch.lolesports.com/vod/104202471517814561/1",
+  // "https://watch.lolesports.com/vod/104202471517814521/1",
+  // "https://watch.lolesports.com/vod/104202471517814529/1",
+  // "https://watch.lolesports.com/vod/104202471517814565/1",
+  // "https://watch.lolesports.com/vod/104202471517814553/1",
+  // "https://watch.lolesports.com/vod/104202471517814541/1",
+  // "https://watch.lolesports.com/vod/104202471517748977/1",
+  // "https://watch.lolesports.com/vod/104202471517814515/1",
+  // "https://watch.lolesports.com/vod/104202471517814533/1",
+  // "https://watch.lolesports.com/vod/104202471517814555/1",
+  // "https://watch.lolesports.com/vod/104202471517814559/1",
+  // "https://watch.lolesports.com/vod/104202471517814519/1",
+  // "https://watch.lolesports.com/vod/104202471517814527/1",
+  // "https://watch.lolesports.com/vod/104202471517814567/1",
+  // "https://watch.lolesports.com/vod/104202471517814551/1"
+
+
+  // "https://watch.lolesports.com/vod/102147202607915021/1",
+  // "https://watch.lolesports.com/vod/102147202603327488/1",
+  // "https://watch.lolesports.com/vod/102147202591793115/1",
+  // "https://watch.lolesports.com/vod/102147202600247286/1",
+  // "https://watch.lolesports.com/vod/102147202594938853/1",
+  // "https://watch.lolesports.com/vod/102147202607325195/1",
+  // "https://watch.lolesports.com/vod/102147202596249577/1",
+  // "https://watch.lolesports.com/vod/102147202588450768/1",
+  // "https://watch.lolesports.com/vod/102147202601426938/1",
+  // "https://watch.lolesports.com/vod/102147202607587340/1",
+  // "https://watch.lolesports.com/vod/102147202597167084/1",
+  // "https://watch.lolesports.com/vod/102147202596904939/1",
+  // "https://watch.lolesports.com/vod/102147202611322904/1",
+  // "https://watch.lolesports.com/vod/102147202603065343/1",
+  // "https://watch.lolesports.com/vod/102147202606014471/1",
+  // "https://watch.lolesports.com/vod/102147202595594215/1",
+  // "https://watch.lolesports.com/vod/102147202587860942/1",
+  // "https://watch.lolesports.com/vod/102147202601754619/1",
+  // "https://watch.lolesports.com/vod/102147202595921896/1",
+  // "https://watch.lolesports.com/vod/102147202594283491/1",
+  // "https://watch.lolesports.com/vod/102147202598084591/1",
+  // "https://watch.lolesports.com/vod/102147202608242702/1",
+  // "https://watch.lolesports.com/vod/102147202602082300/1",
+  // "https://watch.lolesports.com/vod/102147202612895773/1",
+  // "https://watch.lolesports.com/vod/102147202602409981/1",
+  // "https://watch.lolesports.com/vod/102147202589630420/1",
+  // "https://watch.lolesports.com/vod/102147202587205580/1",
+  // "https://watch.lolesports.com/vod/102147202599657460/1",
+  // "https://watch.lolesports.com/vod/102147202593365984/1",
+  // "https://watch.lolesports.com/vod/102147202597494765/1",
+  // "https://watch.lolesports.com/vod/102147202595200998/1",
+  // "https://watch.lolesports.com/vod/102147202610995223/1",
+  // "https://watch.lolesports.com/vod/102147202613223454/1",
+  // "https://watch.lolesports.com/vod/102147202598739953/1",
+  // "https://watch.lolesports.com/vod/102147202593693665/1",
+  // "https://watch.lolesports.com/vod/102147202599329779/1",
+  // "https://watch.lolesports.com/vod/102147202586615754/1",
+  // "https://watch.lolesports.com/vod/102147202586288073/1",
+  // "https://watch.lolesports.com/vod/102147202586943435/1",
+  // "https://watch.lolesports.com/vod/103478355179226682/1",
+  // "https://watch.lolesports.com/vod/103478355179292264/1",
+  // "https://watch.lolesports.com/vod/103478355179226702/1",
+  // "https://watch.lolesports.com/vod/103478355179226698/1",
+  // "https://watch.lolesports.com/vod/103478355179226684/1",
+  // "https://watch.lolesports.com/vod/103478355179226700/1",
+  // "https://watch.lolesports.com/vod/103478355179226708/1",
+  // "https://watch.lolesports.com/vod/103478355179226712/1",
+  // "https://watch.lolesports.com/vod/103478355179226690/1",
+  // "https://watch.lolesports.com/vod/103478355179226716/1",
+  // "https://watch.lolesports.com/vod/103478355179226720/1",
+  // "https://watch.lolesports.com/vod/103478355179226688/1",
+  // "https://watch.lolesports.com/vod/103478355179226714/1",
+  // "https://watch.lolesports.com/vod/103478355179226704/1",
+  // "https://watch.lolesports.com/vod/103478355179161140/1",
+  // "https://watch.lolesports.com/vod/103478355179292266/1",
+  // "https://watch.lolesports.com/vod/103478355179226718/1",
+  // "https://watch.lolesports.com/vod/103478355179226678/1",
+  // "https://watch.lolesports.com/vod/103478355179226722/1",
+  // "https://watch.lolesports.com/vod/103478355179226696/1",
+  // "https://watch.lolesports.com/vod/103478355179226686/1",
+  // "https://watch.lolesports.com/vod/103478355179226692/1",
+  // "https://watch.lolesports.com/vod/103478355179292260/1",
+  // "https://watch.lolesports.com/vod/103478355179226706/1",
+  // "https://watch.lolesports.com/vod/103478355179226694/1",
+  // "https://watch.lolesports.com/vod/103478355179292262/1",
+  // "https://watch.lolesports.com/vod/103478355179226680/1",
+  // "https://watch.lolesports.com/vod/103478355179226710/1",
+  // "https://watch.lolesports.com/vod/103478355179292274/1",
+  // "https://watch.lolesports.com/vod/103478355179292294/1",
+  // "https://watch.lolesports.com/vod/103478355179292320/1",
+  // "https://watch.lolesports.com/vod/103478355179292290/1",
+  // "https://watch.lolesports.com/vod/103478355179292300/1",
+  // "https://watch.lolesports.com/vod/103478355179292304/1",
+  // "https://watch.lolesports.com/vod/103478355179292292/1",
+  // "https://watch.lolesports.com/vod/103478355179292276/1",
+  // "https://watch.lolesports.com/vod/103478355179292308/1",
+  // "https://watch.lolesports.com/vod/103478355179292280/1",
+  // "https://watch.lolesports.com/vod/103478355179292312/1",
+  // "https://watch.lolesports.com/vod/103478355179292282/1",
+  // "https://watch.lolesports.com/vod/103478355179292306/1",
+  // "https://watch.lolesports.com/vod/103478355179292268/1",
+  // "https://watch.lolesports.com/vod/103478355179292322/1",
+  // "https://watch.lolesports.com/vod/103478355179292296/1",
+  // "https://watch.lolesports.com/vod/103478355179292288/1",
+  // "https://watch.lolesports.com/vod/103478355179292314/1",
+  // "https://watch.lolesports.com/vod/103478355179292270/1",
+  // "https://watch.lolesports.com/vod/103478355179292310/1",
+  // "https://watch.lolesports.com/vod/103478355179292316/1",
+  // "https://watch.lolesports.com/vod/103478355179292298/1",
+  // "https://watch.lolesports.com/vod/103478355179292284/1",
+  // "https://watch.lolesports.com/vod/103478355179292278/1",
+  // "https://watch.lolesports.com/vod/103478355179292302/1",
+  // "https://watch.lolesports.com/vod/103478355179292318/1",
+  // "https://watch.lolesports.com/vod/103478355179292272/1",
+  // "https://watch.lolesports.com/vod/103478355179292286/1",
+  // "https://watch.lolesports.com/vod/103478355179292350/1",
+  // "https://watch.lolesports.com/vod/103478355179357912/1",
+  // "https://watch.lolesports.com/vod/103478355179292346/1",
+  // "https://watch.lolesports.com/vod/103478355179292330/1",
+  // "https://watch.lolesports.com/vod/103478355179292348/1",
+  // "https://watch.lolesports.com/vod/103478355179357896/1",
+  // "https://watch.lolesports.com/vod/103478355179292356/1",
+  // "https://watch.lolesports.com/vod/103478355179292332/1",
+  // "https://watch.lolesports.com/vod/103478355179357904/1",
+  // "https://watch.lolesports.com/vod/103478355179357900/1",
+  // "https://watch.lolesports.com/vod/103478355179292336/1",
+  // "https://watch.lolesports.com/vod/103478355179292338/1",
+  // "https://watch.lolesports.com/vod/103478355179357914/1",
+  // "https://watch.lolesports.com/vod/103478355179292324/1",
+  // "https://watch.lolesports.com/vod/103478355179357898/1",
+  // "https://watch.lolesports.com/vod/103478355179292352/1",
+  // "https://watch.lolesports.com/vod/103478355179292344/1",
+  // "https://watch.lolesports.com/vod/103478355179357906/1",
+  // "https://watch.lolesports.com/vod/103478355179292326/1",
+  // "https://watch.lolesports.com/vod/103478355179357902/1",
+  // "https://watch.lolesports.com/vod/103478355179292334/1",
+  // "https://watch.lolesports.com/vod/103478355179292354/1",
+  // "https://watch.lolesports.com/vod/103478355179292340/1",
+  // "https://watch.lolesports.com/vod/103478355179357908/1",
+  // "https://watch.lolesports.com/vod/103478355179292358/1",
+  // "https://watch.lolesports.com/vod/103478355179292342/1",
+  // "https://watch.lolesports.com/vod/103478355179357910/1",
+  // "https://watch.lolesports.com/vod/103478355179292328/1",
+  // "https://watch.lolesports.com/vod/103478355185518301/1",
+  // "https://watch.lolesports.com/vod/103478355185518307/1"
+// ];
+
+// let urls = ["https://watch.lolesports.com/vod/104202471517814517/1", "https://watch.lolesports.com/vod/104202471517814563/1", "https://watch.lolesports.com/vod/104202471517814531/1", "https://watch.lolesports.com/vod/104202471517814547/1", "https://watch.lolesports.com/vod/104202471517814535/1", "https://watch.lolesports.com/vod/104202471517814525/1", "https://watch.lolesports.com/vod/104202471517814549/1", "https://watch.lolesports.com/vod/104202471517814543/1", "https://watch.lolesports.com/vod/104202471517814545/1", "https://watch.lolesports.com/vod/104202471517814561/1", "https://watch.lolesports.com/vod/104202471517814521/1", "https://watch.lolesports.com/vod/104202471517814529/1", "https://watch.lolesports.com/vod/104202471517814565/1", "https://watch.lolesports.com/vod/104202471517814553/1", "https://watch.lolesports.com/vod/104202471517814541/1", "https://watch.lolesports.com/vod/104202471517748977/1", "https://watch.lolesports.com/vod/104202471517814515/1", "https://watch.lolesports.com/vod/104202471517814533/1", "https://watch.lolesports.com/vod/104202471517814555/1", "https://watch.lolesports.com/vod/104202471517814559/1", "https://watch.lolesports.com/vod/104202471517814519/1", "https://watch.lolesports.com/vod/104202471517814527/1", "https://watch.lolesports.com/vod/104202471517814567/1", "https://watch.lolesports.com/vod/104202471517814551/1"];
+let urls = ["https://watch.lolesports.com/vod/104202471517814609/1", "https://watch.lolesports.com/vod/104202471517814569/1", "https://watch.lolesports.com/vod/104202471517814621/1", "https://watch.lolesports.com/vod/104202471517814597/1", "https://watch.lolesports.com/vod/104202471517814611/1", "https://watch.lolesports.com/vod/104202471517814615/1", "https://watch.lolesports.com/vod/104202471517814571/1", "https://watch.lolesports.com/vod/104202471517814589/1", "https://watch.lolesports.com/vod/104202471517814575/1", "https://watch.lolesports.com/vod/104202471517814623/1", "https://watch.lolesports.com/vod/104202471517814583/1", "https://watch.lolesports.com/vod/104202471517814607/1", "https://watch.lolesports.com/vod/104202471517814557/1", "https://watch.lolesports.com/vod/104202471517814539/1", "https://watch.lolesports.com/vod/104202471517814523/1", "https://watch.lolesports.com/vod/104202471517814537/1", "https://watch.lolesports.com/vod/104202471517814517/1", "https://watch.lolesports.com/vod/104202471517814563/1", "https://watch.lolesports.com/vod/104202471517814531/1", "https://watch.lolesports.com/vod/104202471517814547/1", "https://watch.lolesports.com/vod/104202471517814535/1", "https://watch.lolesports.com/vod/104202471517814525/1", "https://watch.lolesports.com/vod/104202471517814549/1", "https://watch.lolesports.com/vod/104202471517814543/1", "https://watch.lolesports.com/vod/104202471517814545/1", "https://watch.lolesports.com/vod/104202471517814561/1", "https://watch.lolesports.com/vod/104202471517814521/1", "https://watch.lolesports.com/vod/104202471517814529/1", "https://watch.lolesports.com/vod/104202471517814565/1", "https://watch.lolesports.com/vod/104202471517814553/1", "https://watch.lolesports.com/vod/104202471517814541/1", "https://watch.lolesports.com/vod/104202471517748977/1", "https://watch.lolesports.com/vod/104202471517814515/1", "https://watch.lolesports.com/vod/104202471517814533/1", "https://watch.lolesports.com/vod/104202471517814555/1", "https://watch.lolesports.com/vod/104202471517814559/1", "https://watch.lolesports.com/vod/104202471517814519/1", "https://watch.lolesports.com/vod/104202471517814527/1", "https://watch.lolesports.com/vod/104202471517814567/1", "https://watch.lolesports.com/vod/104202471517814551/1"];
+
+// const ChromeLauncher = require('chrome-launcher');
+require('console-super');
+
+var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var process = require('process');
+var path = require('path')
+let fs = require('fs');
+let os = require('os');
+
+let i;
+
+if (fs.existsSync("lastIndex")) {
+  i = parseInt(fs.readFileSync('lastIndex'));
+} else {
+  i = urls.length - 1;
+  fs.writeFileSync('lastIndex', i.toString());
+}
+
+let interval = null;
+let child = null;
+
+// function newUrl() {
+//   ChromeLauncher.launch({
+//     startingUrl: 'www.google.com',
+//     userDataDir: path.join("C:","Users","you","AppData","Local","Google","Chrome","User Data"),
+//     ignoreDefaultFlags: true
+//   }).then(chrome => {
+//     console.log(`Chrome debugging port running on ${chrome.port}`);
+//     setTimeout(() => {
+//       console.log('kill?')
+//       chrome.kill();
+//     }, 5000);
+//   });
+//   setTimeout(() => {
+//     ChromeLauncher.killAll();
+//     console.log('killed?')
+//   }, 5000);
+//   i++;
+// }
+
+// newUrl();
+
+interval = setInterval(function loop() {
+  if (i < 0) {
+    clearInterval(interval);
+    console.timeTag('**DONE**', {
+      mod1: 'green'
+    });
+  } else {
+    if (child != null) {
+      console.timeTag('Killing ' + urls[i - 1], {
+        mod1: 'green'
+      });
+      try {
+        execSync('taskkill /IM chrome.exe /F');
+      } catch (e) {
+        console.timeTag('Process already killed.', {
+          mod1: 'yellow'
+        });
+      }
+    }
+    console.timeTag('Opening ' + urls[i]);
+    child = exec('start chrome --new-window ' + urls[i]);
+    fs.writeFileSync('lastIndex', i.toString());
+    i--;
+  }
+  return loop;
+
+}(), 30* 60 * 1000);
